7339e2b2786ea13733232aedde80bf97dfa48f97
Dimitri Dechamp <fablab.fpv@gmail.com>
2021-03-14 10:42:48 +0100

    test5

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 8)+"px; left:"+(e.pageX - 8)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    },500);
+})
